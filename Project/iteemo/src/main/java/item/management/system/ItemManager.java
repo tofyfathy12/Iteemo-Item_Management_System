27b8aab9470bbdd7ba343997a5b05f11bf9ff3fb
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-05-22 17:46:22 +0300

    modified:   Project/iteemo/src/main/java/item/management/system/ItemManager.java 	modified:   Project/iteemo/src/main/java/item/management/system/Main.java 	modified:   Project/iteemo/target/classes/item/management/system/Item.class 	modified:   Project/iteemo/target/classes/item/management/system/ItemManager.class 	modified:   Project/iteemo/target/classes/item/management/system/Main.class

--- Project/iteemo/src/main/java/item/management/system/ItemManager.java
+++ Project/iteemo/src/main/java/item/management/system/ItemManager.java
@@ -1,5 +1,97 @@
 package item.management.system;
 
+class Item {
+    private Integer ID;
+    private String name, desc, category;
+    private boolean priority;
+
+    public Item(Integer id, String name, String desc, String category, boolean priority) {
+        this.ID = id;
+        this.name = name;
+        this.desc = desc;
+        this.category = category;
+        this.priority = priority;
+    }
+
+    public Integer getID() {
+        return this.ID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String newDesc) {
+        this.desc = newDesc;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String newCategory) {
+        this.category = newCategory;
+    }
+
+    public boolean getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(boolean newPriority) {
+        this.priority = newPriority;
+    }
+}
+
 public class ItemManager {
+    DLL<Item> itemsDll = new DLL<Item>();
+    BinarySearchTree<Integer, DLLNode<Item>> bst = new BinarySearchTree<Integer, DLLNode<Item>>();
+
+    public void addItem(String name, String description, String category, String priority) {
+
+    }
+
+    public void viewItemById(int id) {
+
+    }
+
+    public void viewAllItems() {
+
+    }
+
+    public void updateItem(int id, String newName, String newDescription, String newCategory, String newPriority) {
+
+    }
+
+    public void deleteItem(int id) {
+
+    }
+
+    public void undoLastDeletion() {
+
+    }
+
+    public void processNextPriorityItem() { // Dequeue next urgent/normal item
     
+    }
+
+    public void searchItemByName(String name) {
+
+    }
+    public void searchItemByCategory(String category) {
+
+    }
+    public void saveToFile(String filename) {
+
+    }
+    public void loadFromFile(String filename) {
+
+    }
 }
--- Project/iteemo/src/main/java/item/management/system/Main.java
+++ Project/iteemo/src/main/java/item/management/system/Main.java
@@ -1,59 +1,8 @@
 package item.management.system;
 
-class Item {
-    private Integer ID;
-    private String name, desc, category;
-    private boolean priority;
 
-    public Item(Integer id, String name, String desc, String category, boolean priority) {
-        this.ID = id;
-        this.name = name;
-        this.desc = desc;
-        this.category = category;
-        this.priority = priority;
-    }
-
-    public Integer getID() {
-        return this.ID;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String newName) {
-        this.name = newName;
-    }
-
-    public String getDesc() {
-        return this.desc;
-    }
-
-    public void setDesc(String newDesc) {
-        this.desc = newDesc;
-    }
-
-    public String getCategory() {
-        return this.category;
-    }
-
-    public void setCategory(String newCategory) {
-        this.category = newCategory;
-    }
-
-    public boolean getPriority() {
-        return this.priority;
-    }
-
-    public void setPriority(boolean newPriority) {
-        this.priority = newPriority;
-    }
-}
 
 public class Main {
-
-    DLL<Item> itemsDll = new DLL<Item>();
-    BinarySearchTree<Integer, DLLNode<Item>> bst = new BinarySearchTree<Integer, DLLNode<Item>>();
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
