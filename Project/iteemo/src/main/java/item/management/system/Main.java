5f647f6b3784fd8e2e51977595fb57f009dd6f9a
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-05-25 23:12:30 +0300

    modified:   Project/iteemo/src/main/java/item/management/system/Main.java 	modified:   Project/iteemo/target/classes/item/management/system/Main.class

--- Project/iteemo/src/main/java/item/management/system/Main.java
+++ Project/iteemo/src/main/java/item/management/system/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         ItemManager manager = new ItemManager();
-        ConsoleMenu menu = new ConsoleMenu();
         manager.loadFromFile();
         manager.printMenu();
         Scanner scanner = new Scanner(System.in);
@@ -43,7 +42,7 @@ public class Main {
                 System.out.print("Enter ID:");
                 int viewId = scanner.nextInt();
                 
-                // manager.updateItem(viewId, name, description, category, priority);
+                manager.updateItem(viewId, name, description, category, priority);
         }
         scanner.close();
     }
