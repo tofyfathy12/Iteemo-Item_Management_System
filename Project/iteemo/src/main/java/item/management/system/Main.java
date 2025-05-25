c1033385e76e2987d99b3c2cf05470056e15acca
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-05-25 16:22:29 +0300

    Refactor Main class and update welcome message

--- Project/iteemo/src/main/java/item/management/system/Main.java
+++ Project/iteemo/src/main/java/item/management/system/Main.java
@@ -1,10 +1,11 @@
 package item.management.system;
 
-
+// import java.util.Scanner;
 
 public class Main {
-
+    ItemManager manager = new ItemManager();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("----------Welcome to the system----------");
+        System.out.println("\\u2502");
     }
 }
